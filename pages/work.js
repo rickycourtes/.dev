6906820307e3a3e72c868aee45d2ed7cd31b595b
Ricky <5074705+rickycourtes@users.noreply.github.com>
2022-01-11 23:18:24 -0800

    Update work.js

--- pages/work.js
+++ pages/work.js
@@ -1,1 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
+export default function Work() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Work</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
