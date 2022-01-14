1c37b29d400012d0cd6045130c10b2b9286c3155
Ricky Courtes <rickycourtes@gmail.com>
2022-01-14 14:41:37 -0800

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '@styles/globals.scss'
+import { useEffect } from 'react';
 
 function Application({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker
+                .register('/sw.js')
+                .catch((registrationError) => {
+                    console.error('SW registration failed: ', registrationError)
+                })
+        })
+    }
+  }, [])
   return (
     <Component {...pageProps} />
   )
